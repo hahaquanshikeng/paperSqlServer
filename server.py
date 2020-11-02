e43c008595d72918e7388afcad14138225fb19bf
hahaquanshikeng <18225518850@163.com>
2020-11-02 18:26:12 +0800

    temp

---
+++ server.py
@@ -0,0 +1,104 @@
+from flask import Flask, escape, request, abort
+from gevent import pywsgi
+import pymysql
+import json
+import demjson
+# 打开数据库连接
+def getDb():
+    return pymysql.connect("210.30.97.164","papers","issme316","papers" )
+ 
+# # 使用 cursor() 方法创建一个游标对象 cursor
+# cursor = db.cursor()
+ 
+# # 使用 execute()  方法执行 SQL 查询 
+# cursor.execute("SELECT VERSION()")
+ 
+# # 使用 fetchone() 方法获取单条数据.
+# data = cursor.fetchone()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
+
+@app.route('/papers',methods = ['POST'])
+def papers():
+    paperIds = request.form['paperIds']
+    
+    if paperIds.strip()=="":
+        return '[]'
+    db = getDb()
+    cursor = db.cursor()
+    sql = """
+    SELECT 
+        A.cid,
+        A.cdoi,
+        A.ctitle,
+        A.cauth,
+        A.cyear,
+        A.cvenue,
+        A.cabstract,
+        A.nread,
+        COUNT(DISTINCT D.id),
+        C.starNum,
+        C.githubUrl 
+    FROM 
+        clusters as A 
+    LEFT JOIN 
+        papers as B 
+    ON 
+        A.cid = B.cid 
+    LEFT JOIN 
+        github_info as C 
+    ON 
+        B.id = C.paperID
+    LEFT JOIN
+        refs as D
+    ON
+        A.cid = D.cid
+    WHERE 
+        A.cid in ({paperIds})
+    GROUP BY 
+        A.cid""".format(paperIds=paperIds)
+    cursor.execute(sql)
+    listData = cursor.fetchall()
+    result = []
+    for data in listData:
+        try:
+            authData = demjson.decode(data[3])
+            authStr = ",".join([x.get("name","") for x in authData])
+        except Exception:
+            authData = []
+            authStr = None
+        result.append({
+            'id':data[0],
+            'doi':data[1],
+            'title':data[2],
+            'authStr':authStr,
+            'year':data[4],
+            'venue':data[5],
+            'abstract':data[6],
+            'auth':demjson.encode(authData),
+            'view_count':data[7],
+            'start_count':data[9],
+            'source':data[10],
+        })
+    
+    # return sql
+    return json.dumps(result,indent=4)
+
+@app.route('/reference/<id>',methods = ['GET'])
+def reference(id):
+    db = getDb()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM clusters limit 10")
+    data = cursor.fetchone()
+    # print(data)
+    return 'Hello %s!' % data[0]
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',8686,True)
+    # server = pywsgi.WSGIServer(('0.0.0.0',8686), app)
+    # server.serve_forever()
